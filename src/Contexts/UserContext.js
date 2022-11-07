67adf65ec873ce1681d3cb042f76c9f2a03d3c2d
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-07 23:51:39 +0600

    install firebase and set firebase configuration

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -1,15 +1,49 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config';
 
 export const AuthContext = createContext()
 
-// const auth = getAuth(app);
+const auth = getAuth(app);
 const UserContext = ({children}) => {
+    const [loading, setLoading] = useState(true)
     const user = {displayName : "shohug"}
 
+    //01. create user with Email and Password 
+    const createUserWithEmail = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    //02. Login with Email and Password 
+    const logIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+
+    }
+    //03. Log Out
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth)
+    }
+    //04. update user 
+    const updateUser = (name, photo) =>{
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+          }).then(() => {
+            
+          }).catch((error) => {
+            console.log(error?.message);
+          });
+    }
+
     const authInfo = {
-        user
+        user,
+        createUserWithEmail,
+        logIn,
+        logOut,
+        updateUser,
+        
+
     }
     return (
         <AuthContext.Provider value={authInfo}>
