db4d980f23e0eb21e8f167f2b20e0ef47928fa36
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-09 07:34:51 +0600

    create usetitle hooks and set title from different page

--- src/Pages/Service/AllService.js
+++ src/Pages/Service/AllService.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/UserContext';
+import { useTitle } from '../../Hooks/UseTitle';
+import Loader from '../Loader/Loader';
 import SingleService3 from './SingleService3';
 
 const AllService = () => {
+    const { loading } = useContext(AuthContext)
+    useTitle('All Services')
     const services = useLoaderData();
-    return (
-        <div className='my-14'>
+
+    {
+        if (loading) {
+            return <Loader></Loader>
+        }
+        return <div className='my-14'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
                 {
                     services.map(service => <SingleService3 key={service?._id} service={service}></SingleService3>)
                 }
             </div>
         </div>
-    );
+    }
 };
 
 export default AllService;
